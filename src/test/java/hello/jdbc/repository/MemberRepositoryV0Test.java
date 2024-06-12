bd269d6a4f2d93ae8fb13c8a5ea725d29f35454f
kyilfrog <160857010+kyilfrog@users.noreply.github.com>
2024-06-12 20:10:30 +0900

    JDBC 개발 - 조회

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,17 +2,44 @@ package hello.jdbc.repository;
 
 import java.sql.SQLException;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import hello.jdbc.domain.Member;
-
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class MemberRepositoryV0Test {
 
 	MemberRepositoryV0 repository = new MemberRepositoryV0();
 	
 	@Test
 	void crud() throws SQLException {
-		Member member = new Member("memberV1", 10000);
+		//save
+		Member member = new Member("memberV4", 10000);
 		repository.save(member);
+		
+		//findById
+		Member findMember = repository.findById(member.getMemberId());
+		log.info("findMember={}", findMember);
+		log.info("member == findmember {}", member == findMember);
+		log.info("member equals findMember {}", member.equals(findMember));
+		Assertions.assertThat(findMember).isEqualTo(member);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
