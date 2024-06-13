1765d1056e226fdf57f633451c91acbda52ee66c
kyilfrog <160857010+kyilfrog@users.noreply.github.com>
2024-06-13 15:39:03 +0900

    DataSource 예제2 - 커넥션 풀

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -9,6 +9,8 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import lombok.extern.slf4j.Slf4j;
 import static hello.jdbc.connection.ConnectionConst.*;
 
@@ -29,9 +31,24 @@ public class ConnectionTest {
 		useDataSource(dataSource);
 	}
 	
+	@Test
+	void dataSourceConnectionPool() throws SQLException, InterruptedException {
+		//커넥션 풀링
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setJdbcUrl(URL);
+		dataSource.setUsername(USERNAME);
+		dataSource.setPassword(PASSWORD);
+		dataSource.setMaximumPoolSize(10);
+		dataSource.setPoolName("MyPool");
+		
+		useDataSource(dataSource);
+		Thread.sleep(1000);
+	}
+	
 	private void useDataSource(DataSource dataSource) throws SQLException {
 		Connection con1 = dataSource.getConnection();
 		Connection con2 = dataSource.getConnection();
+	
 		log.info("connection={}", "class={}", con1, con1.getClass());
 		log.info("connection={}", "class={}", con2, con2.getClass());
 	}
