b8b68c19b316d8d1154c51bdcf03f6b89bbfe1ea
kyilfrog <160857010+kyilfrog@users.noreply.github.com>
2024-06-22 12:37:22 +0900

    예외 포함과 스택 트레이스

--- src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -7,8 +7,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 
 import hello.jdbc.exception.basic.UnCheckedAppTest.Serivce;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 public class UnCheckedAppTest {
 	
 	@Test
@@ -16,6 +17,16 @@ public class UnCheckedAppTest {
 		Controller controller = new Controller();
 		assertThatThrownBy(() -> controller.request())
 			.isInstanceOf(Exception.class);
+	}
+	
+	@Test
+	void printEx() {
+		Controller controller = new Controller();
+		try {
+			controller.request();
+		} catch (Exception e) {
+			log.info("ex", e);
+		}
 	}
 	
 	static class Controller {
@@ -43,6 +54,14 @@ public class UnCheckedAppTest {
 	}
 	
 	static class Repository {
+		/*
+		 * 1 SQLException이 발생
+		 * 2 catch블록에서 이 예외를 runtimeSQLException으로 감싸서 던짐
+		 * 3 runtimeSQLException의 생성자가 호출되면서 SQLException객체가 cause로 전달됨
+		 * 4 runtimeSQLException의 생성자에서 super(cause)가 호출되어 RuntimeException
+		 *   클래스의 생성자에 cause가 전달된다.
+		 * 5 이렇게 전달된 cause는 RuntimeException 내부에 저장된다.  
+		 */
 		public void call() {
 			try {
 				runSQL();
@@ -63,33 +82,12 @@ public class UnCheckedAppTest {
 	}
 	
 	static class runtimeSQLException extends RuntimeException {
+		//Throwable cause: 예외의 원인을 뜻한다.
+		//super(cause) : RuntimeException(부모 클래스)의 생성자를 호출하는 코드이다.
 		public runtimeSQLException(Throwable cause) {
 			super(cause);
 		}
 	}
 
-			
-		
-	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			
